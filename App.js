25c3f91fa534b29b162b9c68c0296eeee49df7ab
routash <routashsain@gmail.com>
2024-10-11 18:40:59 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 // src/App.js
 import React from 'react';
 import { Navigator } from './src/utils/navigations/Navigators';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 const App = () => (
-  <Navigator />
+  <QueryClientProvider client={queryClient}>
+    <Navigator />
+  </QueryClientProvider>
 );
 
 export default App;
